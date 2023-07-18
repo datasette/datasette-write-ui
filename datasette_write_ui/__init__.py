90d85d8401a299e9eab5e63b413b56e2822d4770
Alex Garcia <alexsebastian.garcia@gmail.com>
2023-07-18 09:03:18 -0700

    new edit, insert, delete UI, edit from client JS

--- datasette_write_ui/__init__.py
+++ datasette_write_ui/__init__.py
@@ -4,54 +4,76 @@ from starlette.requests import Request
 import json
 
 
+async def edit_row_details(scope, receive, datasette, request):
+    db_name = request.args.get("db")
+    table_name = request.args.get("table")
+    pks = request.args.get("primaryKeys")
 
-async def edit(scope, receive, datasette, request):
-  db_name = request.args.get("db")
-  table_name = request.args.get("table")
-  pks = json.loads(request.args.get("pks"))
-  print(db_name, table_name, pks)
+    if not await datasette.permission_allowed(request.actor, 'edit-row', (db_name, table_name)):
+        return Response.json("{}", status=403)
 
-  db = datasette.get_database(db_name)
 
-  columns = []
-  for row in await db.execute("select name, pk, hidden from pragma_table_xinfo(?)", [table_name]):
-    name, pk, hidden = row
-    columns.append({
-      "pk": pk != 0,
-      "name": name,
-      "editable": pk == 0 and hidden not in [2, 3]
-    })##
+    db = datasette.get_database(db_name)
 
-  fields = []
-  select = ", ".join(list(map(lambda column: column.get("name"), columns)))
-  where = f"rowid = {pks[0]}"
-  results = (await db.execute(f"select {select} from {table_name} where {where}"))
+    columns = []
+    for row in await db.execute("select name, pk, hidden from pragma_table_xinfo(?)", [table_name]):
+        name, pk, hidden = row
+        columns.append({
+          "pk": pk != 0,
+          "name": name,
+          "editable": pk == 0 and hidden not in [2, 3]
+        })##
 
-  row = results.first()
-  for column in columns:
-      value = row[column["name"]]
-      print("field:", value, type(value))
-      fields.append({
-        "key": column["name"],
-        "value": value,
-        "type": type(value).__name__,
-        "pk": column["pk"],
-        "editable": column["editable"],
+    column_list = ", ".join(list(map(lambda column: column.get("name"), columns)))
+
+    # TODO only works in single primary key tables
+    results = (await db.execute(f"select {column_list} from {table_name} where rowid = ?", [pks[0]]))
+
+    fields = []
+    row = results.first()
+    for column in columns:
+        value = row[column["name"]]
+        print("field:", value, type(value))
+        fields.append({
+          "key": column["name"],
+          "value": value,
+          "type": type(value).__name__,
+          "pk": column["pk"],
+          "editable": column["editable"],
+        })
+
+    return Response.json({
+          "db": db_name,
+          "table": table_name,
+          "pks": pks,
+          "fields": fields,
       })
 
-  context = {
-      "name": "alex 9",
-      "db": db_name,
-      "table": table_name,
-      "pks": pks,
-      "fields": fields,
-  }
-  return Response.html(
-      await datasette.render_template("edit.html", context, request=request)
-  )
+async def insert_row_details(scope, receive, datasette, request):
+    print(request.actor)
+    db_name = request.args.get("db")
+    table_name = request.args.get("table")
+
+    if not await datasette.permission_allowed(request.actor, 'insert-row', (db_name, table_name)):
+        return Response.json("{}", status=403)
+
+    db = datasette.get_database(db_name)
+
+    insertable_columns = []
+    for row in await db.execute("select name, [type] from pragma_table_xinfo(?) where pk == 0 and hidden == 0", [table_name]):
+        name, type_ = row
+        insertable_columns.append({
+          "name": name,
+          "type": type_
+        })
+
+    return Response.json({
+          "fields": insertable_columns,
+      })
 
 @hookimpl
 def register_routes():
     return [
-        (r"^/-/edit$", edit),
+        (r"^/-/edit-row-details$", edit_row_details),
+        (r"^/-/insert-row-details$", insert_row_details),
     ]
